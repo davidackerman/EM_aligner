3c134f95893eacc766c61290f34e0ddbdc7b2302
khaledkhairy <khaledkhairy@yahoo.com>
2016-09-01 20:29:39 -0400

    faster fusion_collections and longer timeout for webread in get_tile_centers

--- Experiments/Test_Pastix.m
+++ Experiments/Test_Pastix.m
@@ -1,8 +1,6 @@
 %% solve Ax=b using Pastix
-clc
-num_slots = 2;
+
 executable_cmd = sprintf('/groups/flyTEM/home/khairyk/downloads/pastix_gennady/align_tiles_pastix');
-lin_system_fn = '/nobackup/flyTEM/khairy/FAFB00v13/matlab_large_matrix_system/two_tile_matrix_system_Ab.mat';
 mpi_cmd = sprintf('mpirun -np %d %s %s;', num_slots, executable_cmd, lin_system_fn);
 str_env = 'source /groups/flyTEM/home/khairyk/downloads/pastix_gennady/bashrc_for_pastix_20160824';
 %str_env = 'export I_MPI_FABRICS=shm:ofa;export I_MPI_FALLBACK=disable;export LD_LIBRARY_PATH=$HOME/lib64:/usr/local/hwloc-1.11.3/lib:$LD_LIBRARY_PATH';
@@ -11,7 +9,21 @@ str_env = 'source /groups/flyTEM/home/khairyk/downloads/pastix_gennady/bashrc_fo
 
 
 
-str = sprintf('qsub -pe impi %d "%s;%s"', num_slots, str_env, mpi_cmd);
+%str = sprintf('qsub -pe impi %d "%s;%s"', num_slots, str_env, mpi_cmd);
+%%
+clc
+num_slots = 2;
+lin_system_fn = '/nobackup/flyTEM/khairy/FAFB00v13/matlab_large_matrix_system/two_tile_matrix_system_Ab.mat';
+str_pastix_solver = '/nobackup/flyTEM/khairy/FAFB00v13/pastix_solver';
+pastix_script = [str_pastix_solver '/align_tiles_mod01.sh'];
+dir_temp_mx = '/nobackup/flyTEM/khairy/FAFB00v13/matlab_large_matrix_system';
+cd(dir_temp_mx);
+str_env = sprintf('export PASTIX_HOME=%s;export PASTIX_DATA=%s;',str_pastix_solver, dir_temp_mx); 
+str = sprintf('%s%s %d %s', str_env, pastix_script, num_slots, lin_system_fn); 
+disp(str);
+[a, resp_str] = system(str);
+
+%%
 
 %str = sprintf('qlogin -pe impi %d -now no;%s', num_slots, mpi_cmd);
 %s;qsub -l short=true -N %s %s %s -cwd -V -b y -pe batch 1 "%s" 
--- solver/solve_AxB.m
+++ solver/solve_AxB.m
@@ -1,7 +1,7 @@
 function [x2, R] = solve_AxB(K,Lm,options,d)
 % SOLVE
-% Performs the actual solve of the final linear system. 
-% Input: K is a sparse square matrix assembled elsewhere nxn. 
+% Performs the actual solve of the final linear system.
+% Input: K is a sparse square matrix assembled elsewhere nxn.
 %        Lm is a sparse vector nx1
 %        options is a struct with fields:
 %        options.solver: determines the solver strategy. Allowed values
@@ -9,13 +9,13 @@ function [x2, R] = solve_AxB(K,Lm,options,d)
 %           "backslash" : uses Matlab's backslash operator
 %            solving the system K * x2 = Lm directly. This is
 %            memory-limited
-%       
+%
 %           "lsqlin", "gmres", "tfqmr", "bicg", "bicgstab", "lsqr", "pcg",
 %           "symmlq", "cgs", "minres": All of these are iterative methods
 %           that benefit from a good starting value "d" and require
 %           preconditioning. They have tradeoffs regarding accuracy,
 %           efficiency, suitability for a particular system and memory requirements.
-% 
+%
 %          options.L2 and options.U2, if not empty will re-use
 %          preconditioners.
 %
@@ -31,127 +31,136 @@ function [x2, R] = solve_AxB(K,Lm,options,d)
 %
 % Author: Khaled Khairy: Copyright 2016. Janelia Research Campus
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-if ~isfield(options, 'distributed'), options.distributed = 0;end
-disp(['Using distributed: ' num2str(options.distributed)]);
-x2 = [];
-R  = []; 
-
-if options.verbose    % then we also want to know how long it takes to solve the system
-    tic;
-end
-
-%%%%%%%%%%%%%%%%% preconditioning
-if ~strcmp(options.solver,'backslash') ...
-        && ~strcmp(options.solver,'backslash--noreg') ...
-        && ~strcmp(options.solver,'lsqlin')
-    if options.use_ilu && isempty(options.L2)
-        L2 = [];U2 = [];
-        if options.verbose,disp('Calculating preconditioner');end
-        %q = colamd(K);
-        [L2,U2] = ilu(K,struct('type',options.ilu_type,'droptol',options.ilu_droptol, 'udiag', options.ilu_udiag));
-    else
-        L2 = options.L2;
-        U2 = options.U2;
-    end
-    iL2 = L2;
-    iU2 = U2;
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-flag = -999;
-if strcmp(options.solver, 'lsqlin')
-    if options.verbose,disp('------------ Performing backslash x2 = K\Lm solution ');end
-    x2 = lsqlin(A, b, B, d);
+if strcmp(options.solver, 'pastix')
+    if ~isfield(options, 'ncpus'), options.ncpus = 5;end
+    disp(['Using Pastix with: ' num2str(options.ncpus) ' cpus.']);
+    disp(['Matrix A dimensions: ' num2str(size(K,1)) ' x ' num2str(size(K,2))]);
+    disp(['Matrix A non-zeros : ' num2str(nnz(K))]);
+    [x2, R] = solve_pastix(K,Lm,options.ncpus);
     
+else
+    if ~isfield(options, 'distributed'), options.distributed = 0;end
+    disp(['Using distributed: ' num2str(options.distributed)]);
+    x2 = [];
+    R  = [];
     
-elseif strcmp(options.solver, 'backslash')
-    if options.verbose,disp('------------ Performing backslash x2 = K\Lm solution ');end
-    if options.distributed
-        disp('Using distributed Ax=b');
-        x2 = gather(distributed(K)\distributed(Lm));
-    else
-        x2 = K\Lm;
+    if options.verbose    % then we also want to know how long it takes to solve the system
+        tic;
     end
     
-elseif strcmp(options.solver, 'gmres')
-    if options.verbose,disp('------------ Performing gmres');end
-    %x1 = K\Lm; % sosi
-    %[x2,flag,relres,iter,resvec] = gmres(K,Lm,options.restart,options.tol, options.maxit, L2, U2, x1);
-    [x2,flag,relres,iter,resvec] = gmres(K,Lm,options.restart,options.tol, options.maxit,L2, U2, d);
-    disp(flag);
-    % flag = 0
-    % gmres converged to the desired tolerance tol within maxit outer iterations.
-    % flag = 1
-    % gmres iterated maxit times but did not converge.
-    % flag = 2
-    % Preconditioner M was ill-conditioned.
-    % flag = 3
-    % gmres stagnated. (Two consecutive iterates were the same.)
-elseif strcmp(options.solver, 'tfqmr')
-    if options.verbose,disp('------------ Performing tfqmr');end
-    [x2,flag,relres,iter,resvec] = tfqmr(K,Lm,options.tol, options.maxit,L2,U2,d);
+    %%%%%%%%%%%%%%%%% preconditioning
+    if ~strcmp(options.solver,'backslash') ...
+            && ~strcmp(options.solver,'backslash--noreg') ...
+            && ~strcmp(options.solver,'lsqlin')
+        if options.use_ilu && isempty(options.L2)
+            L2 = [];U2 = [];
+            if options.verbose,disp('Calculating preconditioner');end
+            %q = colamd(K);
+            [L2,U2] = ilu(K,struct('type',options.ilu_type,'droptol',options.ilu_droptol, 'udiag', options.ilu_udiag));
+        else
+            L2 = options.L2;
+            U2 = options.U2;
+        end
+        iL2 = L2;
+        iU2 = U2;
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    flag = -999;
+    if strcmp(options.solver, 'lsqlin')
+        if options.verbose,disp('------------ Performing backslash x2 = K\Lm solution ');end
+        x2 = lsqlin(A, b, B, d);
+        
+        
+    elseif strcmp(options.solver, 'backslash')
+        if options.verbose,disp('------------ Performing backslash x2 = K\Lm solution ');end
+        if options.distributed
+            disp('Using distributed Ax=b');
+            x2 = gather(distributed(K)\distributed(Lm));
+        else
+            x2 = K\Lm;
+        end
+        
+    elseif strcmp(options.solver, 'gmres')
+        if options.verbose,disp('------------ Performing gmres');end
+        %x1 = K\Lm; % sosi
+        %[x2,flag,relres,iter,resvec] = gmres(K,Lm,options.restart,options.tol, options.maxit, L2, U2, x1);
+        [x2,flag,relres,iter,resvec] = gmres(K,Lm,options.restart,options.tol, options.maxit,L2, U2, d);
+        disp(flag);
+        % flag = 0
+        % gmres converged to the desired tolerance tol within maxit outer iterations.
+        % flag = 1
+        % gmres iterated maxit times but did not converge.
+        % flag = 2
+        % Preconditioner M was ill-conditioned.
+        % flag = 3
+        % gmres stagnated. (Two consecutive iterates were the same.)
+    elseif strcmp(options.solver, 'tfqmr')
+        if options.verbose,disp('------------ Performing tfqmr');end
+        [x2,flag,relres,iter,resvec] = tfqmr(K,Lm,options.tol, options.maxit,L2,U2,d);
+        
+    elseif strcmp(options.solver, 'bicg')
+        if options.verbose,disp('------------ Performing biconjugate gradients');end
+        [x2,flag,relres,iter,resvec] = bicg(K,Lm,options.tol, options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'bicgstab')
+        if options.verbose,disp('------------ Performing biconjugate gradients stabelized');end
+        [x2,flag,relres,iter,resvec] = bicgstab(K,Lm,options.tol, options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'lsqr')
+        if options.verbose,disp('------------ Performing lsqr');end
+        [x2,flag,relres,iter,resvec] = lsqr(A,b,options.tol,options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'symmlq')
+        if options.verbose,disp('------------ Performing symmlq');end
+        [x2,flag,relres,iter,resvec] = symmlq(K,Lm,options.tol, options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'cgs')
+        if options.verbose,disp('------------ Performing cgs');end
+        [x2,flag,relres,iter,resvec] = cgs(K,Lm,options.tol, options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'pcg')
+        if options.verbose,disp('------------ Performing pcg with incomplete cholesky preconditioning');end
+        [x2,flag,relres,iter,resvec] = pcg(K,Lm,options.tol, options.maxit, L2, U2, d);
+        
+    elseif strcmp(options.solver, 'minres')
+        if options.verbose,disp('------------ Performing minres');end
+        [x2,flag,relres,iter,resvec] = minres(K,Lm,options.tol, options.maxit, L2, U2, d);
+    end
+    if options.verbose
+        disp('-----------    Time ----------');
+        toc
+        disp('------------------------------');
+    end
+    % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% sosi
+    % x1 = K\Lm;
+    % disp(num2str([d(100:130) x1(100:130) x2(100:130)]));
+    % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-elseif strcmp(options.solver, 'bicg')
-    if options.verbose,disp('------------ Performing biconjugate gradients');end
-    [x2,flag,relres,iter,resvec] = bicg(K,Lm,options.tol, options.maxit, L2, U2, d);
+    if options.verbose,
+        disp('Flag (default is -999 for backslash)');disp(flag);
+        disp('Done!');
+    end
+    if ~strcmp(options.solver,'backslash')...
+            && ~strcmp(options.solver,'backslash--noreg')
+        c1 = [0:numel(resvec)-1];
+        c2 = resvec/norm(Lm);
+        R  = [c1(:) c2(:)];
+    else
+        R = K*x2-Lm;
+    end
     
-elseif strcmp(options.solver, 'bicgstab')
-    if options.verbose,disp('------------ Performing biconjugate gradients stabelized');end
-    [x2,flag,relres,iter,resvec] = bicgstab(K,Lm,options.tol, options.maxit, L2, U2, d);
+    if options.debug && ~strcmp(options.solver,'backslash')...
+            && ~strcmp(options.solver,'backslash--noreg')
+        
+        semilogy(R(:,1),R(:,2),'-o');
+        xlabel('Iteration Number');
+        ylabel('Relative Residual');
+        warning on;
+        drawnow;
+        pause(3);
+    end
     
-elseif strcmp(options.solver, 'lsqr')
-    if options.verbose,disp('------------ Performing lsqr');end
-    [x2,flag,relres,iter,resvec] = lsqr(A,b,options.tol,options.maxit, L2, U2, d);
+    x2 = real(x2);
     
-elseif strcmp(options.solver, 'symmlq')
-    if options.verbose,disp('------------ Performing symmlq');end
-    [x2,flag,relres,iter,resvec] = symmlq(K,Lm,options.tol, options.maxit, L2, U2, d);
-    
-elseif strcmp(options.solver, 'cgs')
-    if options.verbose,disp('------------ Performing cgs');end
-    [x2,flag,relres,iter,resvec] = cgs(K,Lm,options.tol, options.maxit, L2, U2, d);
-    
-elseif strcmp(options.solver, 'pcg')
-    if options.verbose,disp('------------ Performing pcg with incomplete cholesky preconditioning');end
-    [x2,flag,relres,iter,resvec] = pcg(K,Lm,options.tol, options.maxit, L2, U2, d);
-    
-elseif strcmp(options.solver, 'minres')
-    if options.verbose,disp('------------ Performing minres');end
-    [x2,flag,relres,iter,resvec] = minres(K,Lm,options.tol, options.maxit, L2, U2, d);
 end
-if options.verbose
-disp('-----------    Time ----------');
-toc
-disp('------------------------------');
-end
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% sosi
-% x1 = K\Lm;
-% disp(num2str([d(100:130) x1(100:130) x2(100:130)]));
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-if options.verbose,
-    disp('Flag (default is -999 for backslash)');disp(flag);
-    disp('Done!');
-end
-if ~strcmp(options.solver,'backslash')...
-        && ~strcmp(options.solver,'backslash--noreg')
-    c1 = [0:numel(resvec)-1];
-    c2 = resvec/norm(Lm);
-    R  = [c1(:) c2(:)];
-else 
-    R = K*x2-Lm;
-end
-
-if options.debug && ~strcmp(options.solver,'backslash')...
-        && ~strcmp(options.solver,'backslash--noreg')
-    
-    semilogy(R(:,1),R(:,2),'-o');
-    xlabel('Iteration Number');
-    ylabel('Relative Residual');
-    warning on;
-    drawnow;
-    pause(3);
-end
-
-x2 = real(x2);
 

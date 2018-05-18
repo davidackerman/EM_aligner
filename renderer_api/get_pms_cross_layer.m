612b34cbff8d2bd8124c5c77d9280cd0a004c37c
Matthew Nichols <mattnichols@gmail.com>
2018-05-18 10:07:08 -0400

    It is trying to do pep8 on matlab code...

--- renderer_api/get_pms_cross_layer.m
+++ renderer_api/get_pms_cross_layer.m
@@ -54,13 +54,13 @@ try
   for pix = numel(pm):-1:1
     if outside_group
       urlChar = sprintf('%s/owner/%s/matchCollection/%s/group/%s/matchesOutsideGroup', ...
-			pm(pix).server, pm(pix).owner,
+			pm(pix).server, pm(pix).owner, ...
 			pm(pix).match_collection, sID1);
       jj =[jj; webread(urlChar, wopts)];
 
     else
       urlChar = sprintf('%s/owner/%s/matchCollection/%s/group/%s/matchesWith/%s', ...
-			pm(pix).server, pm(pix).owner,
+			pm(pix).server, pm(pix).owner, ...
 			pm(pix).match_collection, sID1, sID2);
       jj(pix) = webread(urlChar,wopts)
     end
